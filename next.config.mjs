fb641c70548b1feeba33a1bd90e5c566af8aa83b
devswithme <developswithme@gmail.com>
2025-08-03 23:07:42 +0800

    modify package.json

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ const nextConfig = {
     return webpackConfig
   },
   output: 'standalone',
+  devIndicators: false,
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
